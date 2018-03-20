de2bc5decaddba8c811d343f24e2b497e10cb452
NursuryRhyme <32021152+NursuryRhyme@users.noreply.github.com>
2018-03-20 13:26:51 -0400

    Update project.v
    MakerSpace Progress ecks dee, still gotta figure out how to draw 3x3 we are rn drawing 2x3+1

--- project.v
+++ project.v
@@ -8,6 +8,10 @@
 `include "vga_adapter/vga_controller.v"
 `include "vga_adapter/vga_pll.v"
 
+`define SCREEN_W 8'd159
+`define SCREEN_H 7'd120
+`define PLAYER_SIZE 2'd1
+
 module project
     (
         CLOCK_50,                       //  On Board 50 MHz
@@ -29,7 +33,7 @@ module project
     input           CLOCK_50;               //  50 MHz
     input   [9:0]   SW;
     input   [3:0]   KEY;
-    output [10:0] LEDR;
+    output [17:0] LEDR;
     // Declare your inputs and outputs here
     // Do not change the following outputs
     output          VGA_CLK;                //  VGA Clock
@@ -81,15 +85,16 @@ module project
     assign right = ~KEY[2];
     
     wire load_level, level_pause, play; // game states
-    wire [7:0] playerX, enemyX;
-    wire [6:0] playerY, enemyY;
+    wire [7:0] playerX, playerY, enemyX, enemyY;
     wire [2:0] ani_state;
-    wire player_move, animate_done, player_hit;
+    wire player_move, animate_done;
+	 wire [27:0] count;
     
     // DEBUGGING
-    // assign LEDR[0] = load_level;
-    // assign LEDR[1] = level_pause;
-    // assign LEDR[2] = play;
+    assign LEDR[0] = load_level;
+    assign LEDR[1] = level_pause;
+    assign LEDR[2] = play;
+	 assign LEDR[14:11] = count;
     
     datapath d0(
         .playerX(playerX),
@@ -103,7 +108,8 @@ module project
         .x(x),
         .y(y),
         .colour(colour),
-        .drawEn(writeEn)
+        .drawEn(writeEn),
+		  .counter(count)
     );
     
     control c0(
@@ -134,26 +140,22 @@ module project
         .ani_done(animate_done),
         .resetn(resetn),
         .clk(CLOCK_50),
-        .state(ani_state)
+        .ani_state(ani_state)
     );
     
-    // test 3x3 enemy spawned at (80, 60) moving at 45 degree angle down and to the right
-    // I didn't actually try to draw the enemy yet, only calculated the x and y
+	// test 3x3 enemy spawned at (80, 60) moving at 45 degree angle down and to the right
+	// I didn't actually try to draw the enemy yet, only calculated the x and y
     enemy_control ec0(
         .size(3'd3),
         .start_x(8'd80),
         .start_y(7'd60),
-        .d_x(3'd1),
-        .d_y(3'd1),
-        .leftwards(1'b0),
-        .upwards(1'b0),
-        .playerX(playerX),
-        .playerY(playerY),
-        .load_level(load_level),
+        .d_x(1),
+        .d_y(1),
+        .leftwards(0),
+        .upwards(0),
         .play(play),
         .resetn(resetn),
         .clk(CLOCK_50),
-        .player_hit(player_hit),
         .enemyX(enemyX),
         .enemyY(enemyY)
     );
@@ -221,33 +223,37 @@ module animate_control(
     input ani_done,
     input resetn,
     input clk,
-    output reg [2:0] state
+    output reg [2:0] ani_state
     );
     
     reg [2:0] state_next;
-    localparam IDLE = 3'b000, DRAWPLAYER = 3'b001, LEVEL = 3'b010, ERASEPLAYER = 3'b011, 
+    localparam IDLE = 3'b000, DRAW = 3'b001, LEVEL = 3'b010, ERASE = 3'b011, 
             ERASEtoDRAW = 3'b100; // draw states
     
     always@(*)
     begin: state_table
-        case (state)
+        case (ani_state)
             IDLE: begin
-                if (player_move) state_next <= ERASEPLAYER;
+                if (player_move) state_next <= ERASE;
                 else if (load_level) state_next <= LEVEL;
                 else state_next <= IDLE;
             end
-            DRAWPLAYER: begin
+            DRAW: begin
                 if (ani_done) state_next <= IDLE;
-                else state_next <= DRAWPLAYER;
+                else state_next <= DRAW;
             end
-            ERASEPLAYER: begin
+            ERASE: begin
                 if (ani_done) state_next <= ERASEtoDRAW;
-                else state_next <= ERASEPLAYER;
+                else state_next <= ERASE;
             end
             LEVEL: begin
                 if (ani_done) state_next <= IDLE;
                 else state_next <= LEVEL;
             end
+				ERASEtoDRAW: begin
+				    if (!ani_done) state_next <= DRAW;
+					 else state_next <= DRAW;
+				end
             default: state_next = IDLE;
         endcase
     end // state_table
@@ -256,9 +262,9 @@ module animate_control(
     always @(posedge clk)
     begin: state_FFs
         if(!resetn)
-            state <= IDLE;
+            ani_state <= IDLE;
         else
-            state <= state_next;
+            ani_state <= state_next;
     end // state_FFS
 endmodule
 
@@ -275,14 +281,17 @@ module datapath(
     output reg [7:0] x,
     output reg [6:0] y,
     output reg [2:0] colour,
-    output reg drawEn
+    output reg drawEn,
+	 output reg [27:0] counter
     );
 
-    localparam IDLE = 3'b000, DRAWPLAYER = 3'b001, LEVEL = 3'b010, ERASEPLAYER = 3'b011; // drawing
+    localparam IDLE = 3'b000, DRAW = 3'b001, LEVEL = 3'b010, ERASE = 3'b011; // draw states
+	 //reg [27:0] counter;
     
     initial begin
         colour <= 3'b111;
         ani_done <= 0;
+		  counter <= 0;
     end
     
     always@(posedge clk) begin
@@ -292,29 +301,81 @@ module datapath(
         else if (ani_state == LEVEL) begin
             // TODO: draw the level
             drawEn <= 1;
-            x <= playerX;
-            y <= playerY;
+				colour <= 3'b111;
+				if (counter == 0) begin
+					x <= 8'd80;
+					y <= 7'd100;
+				end
+				else if (counter < 28'd9) begin
+				//else if (counter < (`PLAYER_SIZE * `PLAYER_SIZE)) begin
+					if (x < 8'd80 + 8'd3) begin
+						if (y < 7'd100 + 7'd3) begin
+							y <= y + 7'd1;
+						end
+						else begin
+							y <= 7'd100;
+							x <= x + 8'd1;
+						end
+					end
+				end
+				counter <= counter + 27'd1;
+				
             // when finished drawing, set ani_done to 1
-            ani_done <= 1;
+				if (counter == 28'd10) begin
+					ani_done <= 1;
+				end
         end
-        else if (ani_state == DRAWPLAYER) begin
+        else if (ani_state == DRAW) begin
             // TODO: draw the screen
             drawEn <= 1;
             colour = 3'b111;
-            x <= playerX;
-            y <= playerY;
+				if (counter == 0) begin
+					x <= playerX;
+					y <= playerY;
+				end
+				else if (counter < `PLAYER_SIZE * `PLAYER_SIZE) begin
+					if (x < playerX +`PLAYER_SIZE) begin
+						if (y < playerY + `PLAYER_SIZE) begin
+							y <= y + 1;
+						end
+						else begin
+							y <= playerY;
+							x <= x + 1;
+						end
+					end
+				end
+				counter <= counter + 1;
+				
             // when finished drawing, set ani_done to 1
-            ani_done <= 1;
+				if (counter == `PLAYER_SIZE * `PLAYER_SIZE) begin
+					ani_done <= 1;
+				end
         end
-        else if (ani_state == ERASEPLAYER) begin
+        else if (ani_state == ERASE) begin
             // TODO: erase old screen
             drawEn <= 1;
-            colour = 3'b000;
-            // it should retain its old position at this point
-            //x <= x;
-            //y <= y;
+				colour <= 3'b000;
+				if (counter == 0) begin
+					x <= playerX;
+					y <= playerY;
+				end
+				else if (counter < `PLAYER_SIZE * `PLAYER_SIZE) begin
+					if (x < playerX +`PLAYER_SIZE) begin
+						if (y < playerY + `PLAYER_SIZE) begin
+							y <= y + 1;
+						end
+						else begin
+							y <= playerY;
+							x <= x + 1;
+						end
+					end
+				end
+				counter <= counter + 1;
+				
             // when finished drawing, set ani_done to 1
-            ani_done <= 1;
+				if (counter == `PLAYER_SIZE * `PLAYER_SIZE) begin
+					ani_done <= 1;
+				end
         end
         else begin
             drawEn <= 0;
